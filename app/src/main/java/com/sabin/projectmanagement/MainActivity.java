94767962d325f45c97ef7ce9751c442fc5311bc3
sabind12 <sabind12@gmail.com>
2021-06-14 10:23:48 +0300

    doc first chapters setup

--- app/src/main/java/com/sabin/projectmanagement/MainActivity.java
+++ app/src/main/java/com/sabin/projectmanagement/MainActivity.java
@@ -1,7 +1,6 @@
 package com.sabin.projectmanagement;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +9,6 @@ import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.widget.ViewPager2;
@@ -18,7 +16,6 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {           //Activitatea principala a aplicatiei
 
